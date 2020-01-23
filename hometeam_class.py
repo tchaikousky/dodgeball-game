21bf6ce1a5e8366e95ddb2e4929869bb3aa725ca
Tchaikousky Thomas <tchaikouskythomas@gmail.com>
2020-01-23 15:43:06 -0500

    Added subclass of Player. Added img directory, and attached to Player class.

---
+++ hometeam_class.py
@@ -0,0 +1,24 @@
+import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+class Hometeam(Player):
+    is_special = False
+
+    def special_move(self, is_special):
+        if self.is_special == True:
+            if pressed_keys[K_UP]:
+            self.rect.move_ip(0, -5)
+        if pressed_keys[K_DOWN]:
+            self.rect.move_ip(0, 5)
+        if pressed_keys[K_LEFT]:
+            self.rect.move_ip(-5, 0)
+        if pressed_keys[K_RIGHT]:
+            self.rect.move_ip(5, 0)
---
+++ opposition_class.py
@@ -0,0 +1,12 @@
+import pygame
+from pygame.locals import (
+    K_UP,
+    K_DOWN,
+    K_LEFT,
+    K_RIGHT,
+    K_ESCAPE,
+    KEYDOWN,
+    QUIT,
+)
+
+class Opposition(Player):
